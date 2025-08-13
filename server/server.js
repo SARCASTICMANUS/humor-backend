5bbaa7a072af5ede0048e5bb69b29d5a416ace41
DEEPAK CHAUHAN <108081217+SARCASTICMANUS@users.noreply.github.com>
2025-08-13 21:10:22 +0530

    Update server.js
    origin updated

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:5181'],
+  origin: ['https://humor-backendd.onrender.com/'],
   credentials: true
 }));
 app.use(express.json());
